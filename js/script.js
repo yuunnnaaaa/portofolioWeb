3ba3583b4e48ff822e19441eb70cd3fcbe98015c
yuunnnaaaa <yunitaekasalsabila2863@gmail.com>
2025-10-26 14:37:01 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // typing animation
 var typed = new Typed(".typing",{
-    strings:["Web Designer","Web Developer","UI/UX Designer"],
+    strings:["Junior Web Designer","Junior Web Developer","Junior UI/UX Designer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
